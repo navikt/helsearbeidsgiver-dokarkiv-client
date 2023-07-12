b36b97e5648b7e6995fef90813a1bcae7b5cf090
Mikael Bjerga <mikael.bjerga@nav.no>
2023-07-12 14:38:03 +0200

    Endre rekkefølge på metoder

--- src/main/kotlin/no/nav/helsearbeidsgiver/dokarkiv/DokArkivClient.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/dokarkiv/DokArkivClient.kt
@@ -37,60 +37,52 @@ class DokArkivClient(
 
     private val httpClient = createHttpClient()
 
-    private suspend fun ferdigstill(
-        journalpostId: String,
-        msgId: String,
-        ferdigstillRequest: FerdigstillRequest,
-    ): String {
+    /**
+     * Oppretter en journalpost i Joark/dokarkiv, med eller uten dokumenter.
+     *
+     * Dokumentasjon: [opprettJournalpost](https://confluence.adeo.no/display/BOA/opprettJournalpost)
+     */
+    suspend fun opprettJournalpost(
+        journalpost: OpprettJournalpostRequest,
+        forsoekFerdigstill: Boolean,
+        callId: String,
+    ): OpprettJournalpostResponse {
         try {
-            return httpClient.patch("$url/journalpost/$journalpostId/ferdigstill") {
-                contentType(ContentType.Application.Json)
-                accept(ContentType.Application.Json)
+            return httpClient.post("$url/journalpost?forsoekFerdigstill=$forsoekFerdigstill") {
+                contentType(ContentType.Application.Json.withCharset(Charsets.UTF_8))
                 bearerAuth(getAccessToken())
-                header("Nav-Callid", msgId)
-                setBody(ferdigstillRequest)
-            }
-                .body<String>().also { log.info("ferdigstilling av journalpost ok for journalpostid {}, msgId {}", journalpostId, msgId) }
+                header("Nav-Call-Id", callId)
+                setBody(journalpost)
+            }.body<OpprettJournalpostResponse>()
         } catch (e: Exception) {
-            log.error("Dokarkiv svarte med feilmelding ved ferdigstilling av journalpost for msgId $msgId", e)
             if (e is ClientRequestException) {
-                when (e.response.status) {
-                    HttpStatusCode.NotFound -> {
-                        log.error("Journalposten finnes ikke for journalpostid {}, msgId {}", journalpostId, msgId)
-                        throw RuntimeException("Ferdigstilling: Journalposten finnes ikke for journalpostid $journalpostId msgid $msgId")
-                    }
-
-                    else -> {
-                        log.error("Fikk http status {} for journalpostid {}, msgId {}", e.response.status, journalpostId, msgId)
-                        throw RuntimeException("Fikk feilmelding ved ferdigstilling av journalpostid $journalpostId msgid $msgId")
-                    }
-                }
+                throw DokArkivException(e, e.response.status.value)
             }
-            throw IOException("Dokarkiv svarte med feilmelding ved ferdigstilling av journalpost for $journalpostId msgid $msgId")
+            throw DokArkivException(e)
         }
     }
 
-
-    /**
-     * Tjeneste som lar konsument endre status på en journalpost fra midlerdidig til endelig. Dersom journalposten
-     * ikke er mulig å ferdigstille, for eksempel fordi den mangler påkrevde metadata, får konsument beskjed om hva
-     * som mangler.
-     *
-     * Dokumentasjon: [ferdigstillJournalpost](https://confluence.adeo.no/display/BOA/ferdigstillJournalpost)
-     *
-     * Ved suksessfull ferdigstilling: 200 OK.
-     *
-     * Ved feil:
-     * - 400 Bad Request. Kan ikke ferdigstille. Enten lar ikke journalposten seg ferdigstille eller så er input ugyldig.
-     * - 401 Unauthorized. Konsument kaller tjenesten med ugyldig OIDC-token.
-     * - 403 Forbidden. Konsument har ikke tilgang til å ferdigstille journalpost.
-     * - 500 Internal Server Error. Dersom en uventet feil oppstår i dokarkiv.
-     */
-    suspend fun ferdigstillJournalpost(
+    /** Dokumentasjon: [oppdaterJournalpost](https://confluence.adeo.no/display/BOA/oppdaterJournalpost) */
+    suspend fun oppdaterJournalpost(
         journalpostId: String,
+        fnr: String,
+        isFnr: Boolean,
+        arbeidsgiverNavn: String,
         msgId: String,
-    ): String {
-        return ferdigstill(journalpostId, msgId, FerdigstillRequest(AUTOMATISK_JOURNALFOERING_ENHET))
+    ): HttpResponse {
+        val req = OppdaterJournalpostRequest(
+            bruker = Bruker(
+                fnr,
+                if (isFnr) {
+                    IdType.FNR
+                } else {
+                    IdType.ORGNR
+                },
+            ),
+            avsenderMottaker = AvsenderMottaker(fnr, IdType.FNR, arbeidsgiverNavn),
+            sak = Sak(Sak.SaksType.GENERELL_SAK, "GSAK"),
+        )
+        return oppdater(journalpostId, req, msgId)
     }
 
     private suspend fun oppdater(
@@ -126,51 +118,58 @@ class DokArkivClient(
         }
     }
 
-    /** Dokumentasjon: [oppdaterJournalpost](https://confluence.adeo.no/display/BOA/oppdaterJournalpost) */
-    suspend fun oppdaterJournalpost(
+    /**
+     * Tjeneste som lar konsument endre status på en journalpost fra midlerdidig til endelig. Dersom journalposten
+     * ikke er mulig å ferdigstille, for eksempel fordi den mangler påkrevde metadata, får konsument beskjed om hva
+     * som mangler.
+     *
+     * Dokumentasjon: [ferdigstillJournalpost](https://confluence.adeo.no/display/BOA/ferdigstillJournalpost)
+     *
+     * Ved suksessfull ferdigstilling: 200 OK.
+     *
+     * Ved feil:
+     * - 400 Bad Request. Kan ikke ferdigstille. Enten lar ikke journalposten seg ferdigstille eller så er input ugyldig.
+     * - 401 Unauthorized. Konsument kaller tjenesten med ugyldig OIDC-token.
+     * - 403 Forbidden. Konsument har ikke tilgang til å ferdigstille journalpost.
+     * - 500 Internal Server Error. Dersom en uventet feil oppstår i dokarkiv.
+     */
+    suspend fun ferdigstillJournalpost(
         journalpostId: String,
-        fnr: String,
-        isFnr: Boolean,
-        arbeidsgiverNavn: String,
         msgId: String,
-    ): HttpResponse {
-        val req = OppdaterJournalpostRequest(
-            bruker = Bruker(
-                fnr,
-                if (isFnr) {
-                    IdType.FNR
-                } else {
-                    IdType.ORGNR
-                },
-            ),
-            avsenderMottaker = AvsenderMottaker(fnr, IdType.FNR, arbeidsgiverNavn),
-            sak = Sak(Sak.SaksType.GENERELL_SAK, "GSAK"),
-        )
-        return oppdater(journalpostId, req, msgId)
+    ): String {
+        return ferdigstill(journalpostId, msgId, FerdigstillRequest(AUTOMATISK_JOURNALFOERING_ENHET))
     }
 
-    /**
-     * Oppretter en journalpost i Joark/dokarkiv, med eller uten dokumenter.
-     *
-     * Dokumentasjon: [opprettJournalpost](https://confluence.adeo.no/display/BOA/opprettJournalpost)
-     */
-    suspend fun opprettJournalpost(
-        journalpost: OpprettJournalpostRequest,
-        forsoekFerdigstill: Boolean,
-        callId: String,
-    ): OpprettJournalpostResponse {
+    private suspend fun ferdigstill(
+        journalpostId: String,
+        msgId: String,
+        ferdigstillRequest: FerdigstillRequest,
+    ): String {
         try {
-            return httpClient.post("$url/journalpost?forsoekFerdigstill=$forsoekFerdigstill") {
-                contentType(ContentType.Application.Json.withCharset(Charsets.UTF_8))
+            return httpClient.patch("$url/journalpost/$journalpostId/ferdigstill") {
+                contentType(ContentType.Application.Json)
+                accept(ContentType.Application.Json)
                 bearerAuth(getAccessToken())
-                header("Nav-Call-Id", callId)
-                setBody(journalpost)
-            }.body<OpprettJournalpostResponse>()
-        } catch (e: Exception) {
-            if (e is ClientRequestException) {
-                throw DokArkivException(e, e.response.status.value)
+                header("Nav-Callid", msgId)
+                setBody(ferdigstillRequest)
             }
-            throw DokArkivException(e)
+                .body<String>().also { log.info("ferdigstilling av journalpost ok for journalpostid {}, msgId {}", journalpostId, msgId) }
+        } catch (e: Exception) {
+            log.error("Dokarkiv svarte med feilmelding ved ferdigstilling av journalpost for msgId $msgId", e)
+            if (e is ClientRequestException) {
+                when (e.response.status) {
+                    HttpStatusCode.NotFound -> {
+                        log.error("Journalposten finnes ikke for journalpostid {}, msgId {}", journalpostId, msgId)
+                        throw RuntimeException("Ferdigstilling: Journalposten finnes ikke for journalpostid $journalpostId msgid $msgId")
+                    }
+
+                    else -> {
+                        log.error("Fikk http status {} for journalpostid {}, msgId {}", e.response.status, journalpostId, msgId)
+                        throw RuntimeException("Fikk feilmelding ved ferdigstilling av journalpostid $journalpostId msgid $msgId")
+                    }
+                }
+            }
+            throw IOException("Dokarkiv svarte med feilmelding ved ferdigstilling av journalpost for $journalpostId msgid $msgId")
         }
     }
 }
