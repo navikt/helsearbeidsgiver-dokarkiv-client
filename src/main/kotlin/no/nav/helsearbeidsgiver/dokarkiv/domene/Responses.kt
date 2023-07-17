15b900d5789fa26e6e4c8c17f8695bdc80b458ae
Mikael Bjerga <mikael.bjerga@nav.no>
2023-07-17 10:39:10 +0200

    Fiks respons

--- src/main/kotlin/no/nav/helsearbeidsgiver/dokarkiv/domene/Responses.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/dokarkiv/domene/Responses.kt
@@ -1,12 +1,16 @@
 package no.nav.helsearbeidsgiver.dokarkiv.domene
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonNames
 
 @Serializable
+@OptIn(ExperimentalSerializationApi::class)
 data class OpprettOgFerdigstillResponse(
     val journalpostId: String,
+    @JsonNames("journalpostferdigstilt")
     val journalpostFerdigstilt: Boolean,
-    val melding: String,
+    val melding: String? = null,
     val dokumenter: List<DokumentInfoId>,
 )
 
